78e903c8c7970d120339a8ce59c1804ddc1f0160
Rafael Flores <rafael.flores0801@gmail.com>
2022-06-13 23:46:43 -0600

    Puede que ya lo haya hecho...

--- k_means.py
+++ k_means.py
@@ -1,11 +1,19 @@
 import sys
 import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+def k_means(data, n_cluster):
+    k_pred = KMeans(n_clusters=n_cluster).fit_predict(data)
+    plt.scatter(data[:, 0], data[:, 1], c=k_pred)
+    plt.show()
 
 def main(argv):
     #Something
     file = argv[0]
+    n = int(argv[1])
     data = pd.read_csv(file, header=0).to_numpy()
-    print(type(data))
+    k_means(data, n)
     print("Fin.")
 
 if __name__ == "__main__":
